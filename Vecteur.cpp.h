94336f7cced28262e838837cb3e68fb751ce6317
Robin Reuteler <reutelerr@hotmail.com>
2019-04-04 15:43:24 +0200

    Définitions

--- Vecteur.cpp.h
+++ Vecteur.cpp.h
@@ -5,4 +5,52 @@
 #ifndef LABO4_VECTEUR_CPP_H
 #define LABO4_VECTEUR_CPP_H
 
+#include "Vecteur.h"
+
+template <typename T>
+Vecteur<T>::Vecteur(std::vector<T> v)
+:data(v)
+{}
+
+template <typename T>
+size_t Vecteur<T>::size() const
+{
+    return data.size();
+}
+
+template <typename T>
+T Vecteur<T>::somme() const
+{
+    if(data.size() == 0)
+    {
+        std::exception e; //TODO : Créer classe pour ce type d'exception
+        throw e;
+    }
+        
+    T somme(0);//TODO à tester avec un cas quelconque
+    for (T elem : data)
+    {
+        somme += elem;
+    }
+}
+
+template <typename T> template <typename U>
+Vecteur<T> Vecteur<T>::operator*(U value)
+{
+    if(data.size() == 0)
+    {
+        std::exception e; //TODO : Créer classe pour ce type d'exception
+        throw e;
+    }
+
+    //TODO : Erreur si tableau vide ?
+    for(T elem : data)
+    {
+        elem *= value;
+    }
+}
+
+
+
+
 #endif //LABO4_VECTEUR_CPP_H
