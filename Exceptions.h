0df9c8b8b8eea345d20a6a8d297ca1c695d6b7f7
Robin Reuteler <reutelerr@hotmail.com>
2019-04-04 15:43:43 +0200

    Added Exception.h

---
+++ Exceptions.h
@@ -0,0 +1,13 @@
+//
+//  Exceptions.h
+//  Labo4
+//
+//  Created by Robin Reuteler on 04.04.19.
+//  Copyright © 2019 RobinReuteler. All rights reserved.
+//
+
+#ifndef Exceptions_h
+#define Exceptions_h
+
+
+#endif /* Exceptions_h */
