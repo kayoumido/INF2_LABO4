30a16a2cab17410ae2d94dad5d041f170c41abdc
Doran <dorankayoumi@gmail.com>
2019-04-10 23:04:40 +0200

    Move arithmetic functions
    I forgot to add this file in one of the previous commits

--- Arithmetic.h
+++ Arithmetic.h
@@ -5,4 +5,71 @@
 #ifndef LABO4_ARITHMETIC_H
 #define LABO4_ARITHMETIC_H
 
+#include "Exceptions.h"
+
+template<typename T>
+T multiply(T a, T b) {
+    T result = a * b;
+
+    if (((a >= T() and b >= T()) or (a < T() and b < T())) and result < T()) {
+        throw ArithmeticOverflow(
+                "Vecteur::multiply() - ERROR : Operation caused a memory overflow."
+                " a and b were greater or lower than default value but the result was lower"
+        );
+    }
+
+    if (((a < T() and b >= T()) or (a >= T() and b < T())) and result < T()) {
+        throw ArithmeticOverflow(
+                "Vecteur::multiply() - ERROR : Operation caused a memory overflow."
+                "a is greater and b is lower or a is lower and b greater than the default value "
+                "but the result is lower"
+        );
+    }
+
+    return result;
+}
+
+template<typename T>
+T add(T a, T b) {
+    T result = a + b;
+
+    if (a > T() and b > T() and result < T()) {
+        throw ArithmeticOverflow(
+                "Vecteur::+() - ERROR : Operation caused a memory overflow."
+                " a and b were greater than default value but the result was lower"
+        );
+    }
+
+    if (a < T() and b < T() and result > T()) {
+        throw ArithmeticOverflow(
+                "Vecteur::+() - ERROR : Operation caused a memory overflow."
+                " a and b were lower than default value but the result was greater"
+        );
+    }
+
+    return result;
+}
+
+template<typename T>
+T ubtract(T a, T b) {
+    T result = a - b;
+
+    if (a >= T() and b < T() and result < T()) {
+        throw ArithmeticOverflow(
+                "Vecteur::-() - ERROR : Operation caused a memory overflow"
+                " a is greater and b is lower than the default value but the result is lower"
+        );
+    }
+
+    if (a < T() and b >= T() and result > T()) {
+        throw ArithmeticOverflow(
+                "Vecteur::-() - ERROR : Operation caused a memory overflow"
+                " a is lower and b is greater than the default value but the result is greater"
+        );
+    }
+
+    return result;
+}
+
+
 #endif //LABO4_ARITHMETIC_H
